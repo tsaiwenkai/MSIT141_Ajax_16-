09c5437ae8c5b2971d17f7f7cf95d332f7973d42
tsaiwenkai <t816048we@gmail.com>
2022-07-01 13:12:02 +0800

    加入專案檔案。

---
+++ MSIT141_Ajax_16蔡文楷/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MSIT141_Ajax_16蔡文楷.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
