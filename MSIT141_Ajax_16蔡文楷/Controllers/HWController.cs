c9f71a06229a47e43e097a4fd14f683652534e77
tsaiwenkai <t816048we@gmail.com>
2022-07-03 10:28:31 +0800

    7/3

---
+++ MSIT141_Ajax_16蔡文楷/Controllers/HWController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MSIT141_Ajax_16蔡文楷.Controllers
+{
+    public class HWController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
